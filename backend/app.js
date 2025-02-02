62dcac0b4f4e087f1c3d8bf3d3c0adf783826cb9
AGUSTIN <agustinhillar@hotmail.com>
2025-02-02 19:21:40 -0300

    Deploy

--- backend/app.js
+++ backend/app.js
@@ -475,10 +475,12 @@ async function DBInit() {
 
 };
 
+const PORT = process.env.PORT || 3000;
 
 DBInit().then(() => 
-    app.listen(3001)
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
     )
 
 module.exports = {sequelize, app, Jugadores, Equipos, Partidos, Estadisticas, Estadio, Reserva}
 
+npm install -g serve
